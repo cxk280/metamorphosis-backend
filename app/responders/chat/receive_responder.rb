43f67b130037d4f28a9667d44e8fa658b21c050f
Christopher King <chris@cking.me>
2018-03-09 12:40:04 -0500

    make commit to fill out chat responders from the root directory

---
+++ app/responders/chat/receive_responder.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Receive
+  # Respnder triggered from within the ReceiveConsumer
+  class ReceiveResponder < ApplicationResponder
+    topic :chat
+
+    # @param data [Hash] any data that we want to send
+    # @note Will be serialized to json
+    def respond(data)
+      respond_to :chat, data
+    end
+  end
+end
