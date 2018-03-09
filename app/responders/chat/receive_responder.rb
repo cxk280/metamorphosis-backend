6566f78724c106a9672d6ab262686ed94113c63a
Christopher King <chris@cking.me>
2018-03-09 13:09:39 -0500

    fix some variables in responders

--- app/responders/chat/receive_responder.rb
+++ app/responders/chat/receive_responder.rb
@@ -3,12 +3,12 @@
 module Chat
   # Respnder triggered from within the ReceiveConsumer
   class ReceiveResponder < ApplicationResponder
-    topic :receiveMessage
+    topic :sendMessage
 
     # @param data [Hash] any data that we want to send
     # @note Will be serialized to json
     def respond(data)
-      respond_to :chat, data
+      respond_to :sendMessage, data
     end
   end
 end
--- app/responders/chat/send_responder.rb
+++ app/responders/chat/send_responder.rb
@@ -3,12 +3,12 @@
 module Chat
   # Responder triggered from within the SendConsumer
   class SendResponder < ApplicationResponder
-    topic :sendMessage
+    topic :receiveMessage
 
     # @param data [Hash] any data that we want to send
     # @note Will be serialized to json
     def respond(data)
-      respond_to :chat, data
+      respond_to :receiveMessage, data
     end
   end
 end
